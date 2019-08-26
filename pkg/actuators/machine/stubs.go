8dbd34ff1fd7b292fa2242fdf1613e268ab70b33
xiaobing.meng <xiaobing.meng@alibaba-inc.com>
2019-08-26 18:46:51 +0800

    update project name

--- pkg/actuators/machine/stubs.go
+++ pkg/actuators/machine/stubs.go
@@ -10,8 +10,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	providerconfigv1 "github.com/AliyunContainerService/cluster-api-provider-alicloud/pkg/apis/alicloudprovider/v1alpha1"
-	"github.com/AliyunContainerService/cluster-api-provider-alicloud/test/utils"
+	providerconfigv1 "github.com/AliyunContainerService/cluster-api-provider-alibabacloud/pkg/apis/alicloudprovider/v1alpha1"
+	"github.com/AliyunContainerService/cluster-api-provider-alibabacloud/test/utils"
 	clusterv1 "github.com/openshift/cluster-api/pkg/apis/cluster/v1alpha1"
 	machinev1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
 	machinecontroller "github.com/openshift/cluster-api/pkg/controller/machine"
