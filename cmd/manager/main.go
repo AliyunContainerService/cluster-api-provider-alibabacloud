8d490e31e3cec17992af063ab9ad47d1e138ff2f
xiaobing.meng <xiaobing.meng@alibaba-inc.com>
2021-07-14 19:49:42 +0800

    enable metric

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -128,7 +128,7 @@ func main() {
 
 	enableMetrics := flag.Bool(
 		"enable-metrics",
-		false,
+		true,
 		"Whether to enable metrics, Default value true. If you test in local, you can disable it",
 	)
 
